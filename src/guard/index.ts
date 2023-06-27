720a4af142dcf8b9c5ad108a348ce07941b9d70f
praburangki <praburangki@gmail.com>
2023-06-27 10:03:03 +0530

    feat: add is-string guard

--- src/guard/index.ts
+++ src/guard/index.ts
@@ -2,3 +2,4 @@ export * from './is-array';
 export * from './is-boolean';
 export * from './is-date';
 export * from './is-defined';
+export * from './is-string';
---
+++ src/guard/is-string.spec.ts
@@ -0,0 +1,44 @@
+import { assertType, describe, expect, test } from 'vitest';
+
+import { isString } from './is-string';
+import { typesDataProvider } from '../../test/types-data-provider';
+
+describe('isString', () => {
+  test('isString: should work as type guard', () => {
+    const data = typesDataProvider('string');
+    if (isString(data)) {
+      expect(typeof data).toEqual('string');
+      assertType<string>(data);
+    }
+  });
+  test('isString: should work even if data type is unknown', () => {
+    const data: unknown = typesDataProvider('string');
+    if (isString(data)) {
+      expect(typeof data).toEqual('string');
+      assertType<string>(data);
+    }
+  });
+
+  test('isString: should work with literal types', () => {
+    const data = (): 'a' | 'b' | 'c' | number => {
+      return 'a';
+    };
+    const x = data();
+    if (isString(x)) {
+      expect(typeof x).toEqual('string');
+      assertType<'a' | 'b' | 'c'>(x);
+    }
+  });
+  test('isString: should work as type guard in array', () => {
+    const data = [
+      typesDataProvider('error'),
+      typesDataProvider('string'),
+      typesDataProvider('function'),
+      typesDataProvider('null'),
+      typesDataProvider('array'),
+      typesDataProvider('boolean'),
+    ].filter(isString);
+    expect(data.every((c) => typeof c === 'string')).toEqual(true);
+    assertType<Array<string>>(data);
+  });
+});
