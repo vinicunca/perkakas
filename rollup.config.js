e3139e667e134998935a9bc9ad7b1c3023750095
praburangki <praburangki@gmail.com>
2024-03-28 16:42:02 +0800

    chore: enable cjs file

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
@@ -16,6 +18,8 @@ const plugins = [
   resolve({
     preferBuiltins: true,
   }),
+  json(),
+  commonjs(),
   esbuild({
     target: 'node14',
   }),
@@ -28,6 +32,10 @@ export default [
       {
         file: input.replace('src/', 'dist/').replace('.ts', '.mjs'),
         format: 'esm',
+      },
+      {
+        file: input.replace('src/', 'dist/').replace('.ts', '.cjs'),
+        format: 'cjs',
       },
     ],
     plugins,
