7576d58e5236c02bfcb698b8423b31ded74e4123
praburangki <praburangki@gmail.com>
2024-07-05 10:30:57 +0800

    chore: fix typedoc

--- src/is-object-type.ts
+++ src/is-object-type.ts
@@ -3,9 +3,11 @@ import type { NarrowedTo } from './helpers/types';
 /**
  * Checks if the given parameter is of type `"object"` via `typeof`, excluding `null`.
  *
- * It's important to note that in JavaScript, many entities are considered objects, like Arrays, Classes, RegExps, Maps, Sets, Dates, URLs, Promise, Errors, and more. Although technically an object too, `null` is not considered an object by this function, so that its easier to narrow nullables.
+ * It's important to note that in JavaScript, many entities are considered objects, like Arrays, Classes, RegExps, Maps, Sets, Dates, URLs, Promise, Errors, and more.\
+ * Although technically an object too, `null` is not considered an object by this function, so that its easier to narrow nullables.
  *
- * For a more specific check that is limited to plain objects (simple struct/shape/record-like objects), consider using `isPlainObject` instead. For a simpler check that only removes `null` from the type prefer `isNonNull` or `isDefined`.
+ * For a more specific check that is limited to plain objects (simple struct/shape/record-like objects), consider using `isPlainObject` instead.
+ * For a simpler check that only removes `null` from the type prefer `isNonNull` or `isDefined`.
  *
  * @param data - The variable to be checked for being an object type.
  * @returns The input type, narrowed to only objects.
@@ -25,6 +27,8 @@ import type { NarrowedTo } from './helpers/types';
  * @dataFirst
  * @category Guard
  */
-export const isObjectType = <T>(
+export function isObjectType<T>(
   data: T | object,
-): data is NarrowedTo<T, object> => typeof data === 'object' && data !== null;
+): data is NarrowedTo<T, object> {
+  return typeof data === 'object' && data !== null;
+}
